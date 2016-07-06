af9b3dbd5bd2878bfa09efe65401c130d5df3a77
Tharanipriya21 <tharanipriya900@gmail.com>
2016-07-06 16:05:14 +0530

    Create largrst.c

---
+++ largrst.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the values of a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+printf("a is greater");
+else if((b>c)&&(b>a))
+printf("b is greater");
+else
+printf("c is greater");
+getch();
+}
+
